3b3582bbbde39325c7a83dbae2e6b372cdbdcea8
Raihanroo <raihanroo21@gmail.com>
2024-03-30 11:21:32 +0300

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMWNFBFm7dAjiUEV11tdBwWJTRR11ffVs",
+  authDomain: "auth-firebase-contex-tai-e08e2.firebaseapp.com",
+  projectId: "auth-firebase-contex-tai-e08e2",
+  storageBucket: "auth-firebase-contex-tai-e08e2.appspot.com",
+  messagingSenderId: "336696409705",
+  appId: "1:336696409705:web:2e0ba50af99842846c2cf8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/providers/AuthProviders.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+ 
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+
+const AuthProviders = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    const createUser = (email, password) =>{
+        // return createUserWithEmailAndPassword(auth, email, password);
+        console.log('working');
+    }
+
+    const authInfo = {
+        user ,
+        createUser
+    } 
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;

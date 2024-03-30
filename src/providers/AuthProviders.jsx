fdf0db083b004600c95ec9408becc281b42abd9a
Raihanroo <raihanroo21@gmail.com>
2024-03-30 15:34:20 +0300

    secon-face

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -9,18 +9,16 @@ const auth = getAuth(app);
 
 
 const AuthProviders = ({children}) => {
+    
     const [user, setUser] = useState(null);
 
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password);
         
-<<<<<<< HEAD
     }
 
     const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
-=======
->>>>>>> 11751dc87089a848288f7d08d29d89c83f365fe5
     }
 
     const authInfo = {

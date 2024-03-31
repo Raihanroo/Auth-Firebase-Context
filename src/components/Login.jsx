87aa301ced4b571ad0b3a6396a0120ff251d60c6
Raihanroo <raihanroo21@gmail.com>
2024-03-31 17:08:59 +0300

    Auth-signinwithGoogle

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,44 @@
-
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProviders";
 
 const Login = () => {
-  
-  const { signIn} = useContext(AuthContext);
+  const { signIn, signInWithGoole } = useContext(AuthContext);
 
-  const handleLogin = event => {
+  const handleLogin = (event) => {
     event.preventDefault();
+
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
 
     signIn(email, password)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        form.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGoogleSignIn = () =>{
+    signInWithGoole()
     .then(result => {
       const loggedUser = result.user;
       console.log(loggedUser);
     })
     .catch(error => {
-      console.log(error);
-    } )
+        console.log(error)
+    })
   }
-
-
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col">
         <div className="text-center ">
           <h1 className="text-5xl font-bold">Please Login!</h1>
-       
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleLogin} className="card-body">
@@ -39,7 +47,8 @@ const Login = () => {
                 <span className="label-text">Email</span>
               </label>
               <input
-                type="email" name="email"
+                type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -50,7 +59,8 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="password" name="password"
+                type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
@@ -66,8 +76,13 @@ const Login = () => {
             </div>
           </form>
           <p className="mb-4 ml-8">
-          <Link to='/register' className="label-text-alt link link-hover">New to Auth Master? Pleace Register</Link>
+            <Link to="/register" className="label-text-alt link link-hover">
+              New to Auth Master? Pleace Register
+            </Link>
           </p>
+          <div>
+          <button onClick={handleGoogleSignIn} className="btn btn-primary">Google</button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Orders.jsx
@@ -0,0 +1,11 @@
+
+
+const Orders = () => {
+    return (
+        <div>
+            <h1>This is My Orders! The Orders is here!!!</h1>
+        </div>
+    );
+};
+
+export default Orders;
